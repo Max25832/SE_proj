e8108b438d600d6cd0e1b577c5ba8f250a24ec45
Max25832 <115776441+Max25832@users.noreply.github.com>
2023-01-24 14:35:10 +0100

    Button GUI

---
+++ Project07_01/Sketchbook/RoundedBorder.java
@@ -0,0 +1,31 @@
+package Sketchbook;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.border.LineBorder;
+
+class RoundedBorder extends LineBorder {
+
+    RoundedBorder(int radius) {
+        super(Color.BLACK, 2, true);
+        this.radius = radius;
+    }
+
+    private int radius;
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getLineColor());
+        g2.setStroke(new BasicStroke(getThickness()));
+        g2.drawRoundRect(x, y, width-1, height-1, radius, radius);
+    }
+
+
+}
